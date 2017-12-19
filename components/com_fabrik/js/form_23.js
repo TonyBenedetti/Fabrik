033211d94f867d031a77f1a2c03381d59fff82cf
Tony Benedetti <tony@benedetti.us>
2017-12-18 21:14:53 -0500

    form_23 4

--- components/com_fabrik/js/form_23.js
+++ components/com_fabrik/js/form_23.js
@@ -1,4 +1,82 @@
 /**
+ * The form has just been loaded or the user has just chosen a division of a year:
+ * -- division possibilities are: month, quarter, third, half & season
+ * -- change the label on the "_choice" element
+ * -- update "_choice" element with the value that may have been previously saved
+ *
+ * @param {Object} thisElement - Fabrik element that called us via onLoad or onClick.
+ */
+function dateDivision(thisElement) {
+   var thisForm, elementFullname;
+   var tableName, elementName, tabName, nameRoot;
+   var divisionName, division;
+   var saverName, saverValue, activeName, value;
+   var suffixes, suffix;
+   
+   thisForm        = thisElement.form;
+   elementFullname = thisElement.options.fullName;
+
+   elementName = String(elementFullname).match(/[a-zA-Z0-9]+___(\w+)/)[1];
+   tableName   = String(elementFullname).match(/(\w+)___\w+/)[1];
+   tabName     = String(elementName).match(/(\w+)_\w+/)[1];
+   nameRoot    = tableName + '___' + tabName + '_';
+   
+   divisionLabel = thisForm.elements.get(elementFullname).getValue();
+   divisionName  = divisionName.toLowerCase();
+
+   suffixes = ['_choice', '_accuracy', '_confidence'];
+   suffixes.forEach(function(suffix) {
+      saverName  = nameRoot + 'saver_' + division + '_' + suffix;
+      saverValue = thisForm.elements.get(saverName).getValue();
+
+      activeName  = nameRoot + divisionName + '_' + suffix;
+      
+      if (suffix == '_choice') {
+         thisForm.elements.get(activeName).setLabel(divisionLabel);
+      }
+      thisForm.elements.get(activeName).update(saverValue);
+   });
+   return;
+}
+
+
+/**
+ * The form has just been loaded or the user has just chosen 
+ * a value for this division of a year:
+ * -- division possibilities are: month, quarter, third, half & season
+ * -- stow away that value in the corresponding "_saver_" element
+ *
+ * @param {Object} thisElement - Fabrik element that called us via onLoad or onClick.
+ */
+function dateDivisionChoice(thisElement) {
+   var thisForm, elementFullname,, nameRoot;
+   var elementName, tableName, tabName;
+   var divisionLabel, divisionName;
+   
+   thisForm        = thisElement.form;
+   elementFullname = thisElement.options.fullName;                 // gn_event___start_division_xxx
+   
+   elementName = elementFullname.match(/[a-zA-Z0-9]+___(\w+)/)[1]; // start_division_suffix
+   tableName   = elementFullname.match(/(\w+)___\w+/)[1];          // gn_event
+   tabName     = elementName.match(/(\w+)_\w+/)[1];                // start
+   nameRoot    = tableName + '___' + tabName + '_' + ;
+   
+   divisionLabel = thisForm.elements.get(elementFullname).getValue();
+   divisionName  = divisionLabel.toLowerCase();
+
+   suffixes = ['_choice', '_accuracy', '_confidence'];
+   suffixes.forEach(function(suffix) {
+      saverName   = nameRoot + 'saver_' + divisionName + '_' + suffix;
+      saverValue  = thisForm.elements.get(saverName).getValue();
+      activeName  = nameRoot + 'division_' + suffix;
+      activeValue = thisForm.elements.get(activeName).getValue();
+      thisForm.elements.get(saverName).update(activeValue);
+   }
+   return;
+}
+
+
+/**
  * Show/hide combinations of the Fabrik groups
  * (and their tabs) used to develop a date or
  * duration based on the chosen date type
@@ -13,11 +91,9 @@
 function dateType(thisElement)
 {
    var thisForm        = thisElement.form;
-/*alert('groups: |' + thisForm.groups + '|');*/
    var elementFullname = String(thisElement.options.fullName);
-/*alert('dateType elementFullname: ' + elementFullname);*/
-   var tableName       = String(elementFullname).match(/(\w+)___\w+/)[1];
    var elementName     = String(elementFullname).match(/[a-zA-Z0-9]+___(\w+)/)[1];
+   var tableName       = String(elementFullname).match(/(\w+)___\w+/)[1];
    var tabName         = String(elementName).match(/(\w+)_\w+/)[1];
 
    var chosen = thisForm.elements.get(elementFullname).getValue();
@@ -70,120 +146,3 @@ function dateType(thisElement)
    }
    return;
 }
-
-
-
-/**
- * The form has just been loaded or the user has just chosen a division of a year:
- * -- division possibilities are: month, quarter, third, half & season
- * -- change the label on the "_choice" element
- * -- update "_choice" element with the value that may have been previously saved
- *
- * @param {Object} thisElement - Fabrik element that called us via onLoad or onClick.
- */
-function dateDivision(thisElement) {
-   var thisForm, elementFullname;
-   var tableName, elementName, tabName, nameRoot;
-   var divisionName, division;
-   var saverName, saverValue, valueName, value;
-   var suffixes, suffix;
-   
-   thisForm        = thisElement.form;
-   elementFullname = thisElement.options.fullName;
-
-   tableName   = String(elementFullname).match(/(\w+)___\w+/)[1];
-   elementName = String(elementFullname).match(/[a-zA-Z0-9]+___(\w+)/)[1];
-   tabName     = String(elementName).match(/(\w+)_\w+/)[1];
-   nameRoot    = tableName + '___' + tabName + '_';
-   
-   divisionLabel = thisForm.elements.get(elementFullname).getValue();
-   divisionName  = divisionName.toLowerCase();
-
-   suffixes = ['_choice', '_accuracy', '_confidence'];
-   suffixes.forEach(function(suffix) {
-      saverName  = nameRoot + 'saver_' + division + '_' + suffix;
-      saverValue = thisForm.elements.get(saverName).getValue();
-
-      activeName  = nameRoot + divisionName + '_' + suffix;
-      
-      if (suffix == '_choice') {
-         thisForm.elements.get(activeName).setLabel(divisionLabel);
-      }
-      thisForm.elements.get(activeName).update(saverValue);
-   });
-   return;
-}
-
-
-/**
- * The form has just been loaded or the user has just chosen 
- * a value for this division of a year:
- * -- division possibilities are: month, quarter, third, half & season
- * -- stow away that value in the corresponding "_saver_" element
- *
- * @param {Object} thisElement - Fabrik element that called us via onLoad or onClick.
- */
-function dateDivisionChoice(thisElement) {
-   var thisForm, elementFullname, tableName, elementName, tabName;
-   var divisionName, division;
-   
-   thisForm        = thisElement.form;
-   elementFullname = String(thisElement.options.fullName);
-/* alert('dateDivisionChoice elementFullname: ' + elementFullname); */
-   tableName       = String(elementFullname).match(/(\w+)___\w+/)[1];
-   elementName     = String(elementFullname).match(/[a-zA-Z0-9]+___(\w+)/)[1];
-   tabName         = String(elementName).match(/(\w+)_\w+/)[1];
-
-   divisionName = tableName + '___' + tabName + '_division';
-   division     = String(thisForm.elements.get(divisionName).getValue().toLowerCase());
-
-   valueName = divisionName + '_choice';
-   saverName = String(divisionName + '_saver_' + division);
-   value     = String(thisForm.elements.get(valueName).getValue());
-   thisForm.elements.get(saverName).update(value);
-   
-   valueName = divisionName + '_accuracy';
-   saverName = String(divisionName + '_saver_' + division + '_accuracy');
-   value     = String(thisForm.elements.get(valueName).getValue());
-   thisForm.elements.get(saverName).update(value);
-   
-   valueName = divisionName + '_confidence';
-   saverName = String(divisionName + '_saver_' + division + '_confidence');
-   value     = String(thisForm.elements.get(valueName).getValue());
-   thisForm.elements.get(saverName).update(value);
-
-   return;
-}
-
-/* ========================================================== */
-/* ========================================================== */
-/* ========================================================== */
-/* ========================================================== */
-/* ========================================================== */
-
-
-/*
-function tabShowHide(actions, groups, clickGroup)
-{
-   for (i = 0; i < 3; i++) {
-      tab = '#group' + groups[i] + '_tab';
-      jQuery(tab).hide();
-   }
-   jQuery(clickGroup).trigger('click');
-   return;
-}
-*/
-
-
-   /*
-   // learn group numbers and give them names
-   // including click/default
-   groups = [118, 122, 121];
-
-   switch(chosen) {
-      case choice6 :
-         actions = ['hide', 'show', 'hide'];
-         self::tabShowHide(actions, groups[2]);
-         break;
-   }
-   */
