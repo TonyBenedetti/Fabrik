7712cc33dd2990dc85321e414b23e3abfd299364
Tony Benedetti <Tony@Benedetti.us>
2018-01-02 16:40:53 -0500

    ????

--- _gollynums/gn_common.php
+++ _gollynums/gn_common.php
@@ -77,7 +77,8 @@ class EDTF {
                 $div  = self::buildSegment($data, $tabName, $calType, 'division', $leapYear, 2);
                 $day  = self::buildSegment($data, $tabName, $calType, 'day',      $leapYear, 2);
 				/* alert(gettype($day) . '|' . $day . '|'); */
-				$edtf = $year . '-' . $div . (preg_match('/[\dx]+/', $day) ? '' : ('-' . $day));
+				/*$edtf = $year . '-' . $div . (preg_match('/[\dx]+/', $day) ? '' : ('-' . $day)); */
+				$edtf = $year . '-' . $div . '-' . $day;
                 break;
             case 'iso-yd':
                 $year = self::buildSegment($data, $tabName, $calType, 'year', $leapYear, 0);
