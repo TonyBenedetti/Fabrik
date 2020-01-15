39439b97be4cf700ef48987bdd0af8ea3fe83b02
Tony Benedetti <Tony@Benedetti.us>
2020-01-15 00:11:37 -0500

    Update config.php

--- _github/inc/config.php
+++ _github/inc/config.php
@@ -4,7 +4,7 @@
 $config = array(
 
     // The directory where your website and git repository are located, can be a relative or absolute path
-    'directory' => '/home/greeng42/gollynums.com',
+    'directory' => '/home/benede8/gollynums.com',
 
     // Used for first clone
     'url' => 'https://TonyBenedetti:6150Wilston@github.com/TonyBenedetti/GollyNums.git',
